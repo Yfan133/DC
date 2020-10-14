1e8540ee3ad17ddf72ff8605c882940472dda892
Yfan133 <1157346001@qq.com>
2020-10-14 23:23:09 +0800

    Linux父子进程，僵尸进程 C++拷贝构造函数，运算操作符的重载

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -1,31 +1,39 @@
 #include"main.h"
-class String
+//this指针的生命周期比函数要长
+class Data
 {
 public:
-	String(int capicity = 10)
+	bool operator==(const Data& d)	//运算符重载
 	{
-		_pData = (int*)malloc(sizeof(int) * capicity);
+		return _year == d._year &&
+			_month == d._month &&
+			_day == d._day;
 	}
-	~String()
+	Data& operator+=(const Data& d)
 	{
-		if (_pData)
-		{
-			free(_pData);
-			_pData = nullptr;
-		}
+
+	}
+	Data operator+(/*Data* const this,*/int days)
+	{
+		Data temp(*this);
+		temp._day += days;
+		return temp;
 	}
 private:
-	int* _pData;
+	int _year;
+	int _month;
+	int _day;
 };
-class 
+
+Data operator-(const Data& d, int days)  //全局运算符重载，全局的没有this指针，所以必须给const Data& d,类似Data* const this
 {
-private:
-	String _person;
-	int _age;
-};
+	Data temp(d);
+
+}
+//operator不能重载
 void Test()
 {
-	String d1;
+
 }
 int main()
 {
