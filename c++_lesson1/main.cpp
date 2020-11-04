afb3790902a83398c851dd8f91ced0e6f295cca6
Yfan133 <1157346001@qq.com>
2020-11-04 22:36:30 +0800

    修正了对命名管道的认知

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -5,59 +5,74 @@
 //class Seqlist
 //{
 //public:
-//	Seqlist(int capacity)				//构造
+//	Seqlist(int capacity = 5)				//构造
 //		:_base(new T[capacity])
-//		,_capacity(capacity)
-//		,_size(0)
+//		, _capacity(capacity)
+//		, _size(0)
 //	{}
-//	Seqlist(const Seqlist<T>& tmp);		//拷贝构造
-//	~Seqlist()
-//	{
-//		delete[] _base;
-//		_capacity = 0;
-//		_size = 0;
-//	}
+//	Seqlist(const Seqlist<T>& tmp)			//拷贝构造
+//	{}		
+//	~Seqlist();								//析构函数在类外定义
 //	Seqlist<T>& operator=(const Seqlist<T>& tmp)	//赋值运算符重载
 //	{}
-//	T& operator[](int data)
+//	T& operator[](int data)							//操作符[]重载，base[n]的函数
 //	{
+//		assert(data < _size);
 //		return _base[data];
 //	}
-//	const T& operator[](const T data)const					//操作符[]重载，base[n]的函数
-//	{
-//		return _base[data];
-//	}
-//	void SeqPush(const T data)				//尾插
+//	void SeqPush(const T data)						//尾插
 //	{
 //		_base[_size] = data;
 //		_size++;
+//	}
+//	void SeqPopBack()
+//	{
+//		_size--;
+//	}
+//	int SeqSize()
+//	{
+//		return _size;
 //	}
 //private:
 //	T* _base;
 //	int _size;
 //	int _capacity;
 //};
-//
+//template<class T>
+//Seqlist<T>::~Seqlist()	//在类外实现定义的函数，需要加模板参数列表
+//{
+//	if (_base)
+//	{
+//		delete[] _base;
+//		_capacity = 0;
+//		_size = 0;
+//	}
+//}
 //int main()
 //{
 //	Seqlist<int> s1(10);
+//	Seqlist<double>s2(10);
 //	s1.SeqPush(1);
-//	s1.SeqPush(2);
-//	s1.SeqPush(3);
-//	s1.SeqPush(4);
-//	cout << s1[2] << endl;
+//	s2.SeqPush(1.0);
 //	return 0;
 //}
-#include<string>
 int main()
 {
-	string a("hello world");
-	a.resize(15, 'p');
-	a.resize(10, 'p');
-	a.resize(5);
-	a.reserve(15);
-	a.reserve(5);
-	a.erase(a.begin(), a.begin() + 3);
-	//为什么我这里没有减小容量
+	int arr[100];
+	memset(arr, 0, sizeof(arr));
 	return 0;
 }
+
+//#include<string>
+//int main()
+//{
+//	string a("hello world");
+//	a.resize(15, 'p');
+//	a.resize(10, 'p');
+//	a.resize(5);
+//	a.reserve(15);
+//	a.reserve(5);
+//	a.erase(a.begin(), a.begin() + 3);
+//	//为什么我这里没有减小容量
+//	return 0;
+//}
--- c++_lesson1/main.h
+++ c++_lesson1/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 #include<iostream>
+#include<assert.h>
 using namespace std;	//命名空间中内容全部"全局化"
 #pragma warning (disable:4996)
 #endif // !_MAIN_H_
