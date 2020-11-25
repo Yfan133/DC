f7a2159d02b338a0c42dd0d8c59c35097786593a
Yfan133 <1157346001@qq.com>
2020-11-25 21:09:04 +0800

    网络

--- DS/测试/main.cpp
+++ DS/测试/main.cpp
@@ -19,27 +19,40 @@ using namespace std;
 //	QuickSort(arr, 0, arr.size());
 //	return 0;
 //}
-void fun(int a[], char b[]) //这里是char*和int*
-{
-    a[0] = 2;
-    b[0] = 'w';
-    printf("%d %s", a[0], b);
-}
-class A
-{
-    unsigned a : 29;
-    //unsigned b : 11;
-    //unsigned c : 4;
-    //unsigned d : 29;
-    char index;
-};
+
+    vector<double> dicesProbability(int n)
+    {
+        int dp[15][70];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 1; i <= 6; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++)               //n=2
+        {
+            for (int j = i; j <= 6 * i; j++)         //从2开始到12结束。
+            {
+                for (int cur = 1; cur <= 6; cur++)
+                {
+                    if (j - cur <= 0)
+                    {
+                        break;
+                    }
+                    dp[i][j] += dp[i - 1][j - cur];
+                }
+            }
+        }
+        int all = pow(6, n);
+        vector<double> ret;
+        for (int i = n; i <= 6 * n; i++) 
+        {
+            ret.push_back(dp[n][i] * 1.0 / all);
+        }
+        return ret;
+    }
+
 int main()
 {
-    string ar("qwerasdf");
-    string br;
-    
-    br +=ar.substr(4);
-    ar.erase(2, 3);
-
+    dicesProbability(2);
     return 0;
 }
+
