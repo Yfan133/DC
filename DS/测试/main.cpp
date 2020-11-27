3f36b252ad66d3a7a173cb96b805790050dbe5b2
Yfan133 <1157346001@qq.com>
2020-11-27 22:10:44 +0800

    oj

--- DS/测试/main.cpp
+++ DS/测试/main.cpp
@@ -19,46 +19,50 @@ using namespace std;
 //	QuickSort(arr, 0, arr.size());
 //	return 0;
 //}
-#include<assert.h>
-typedef char BinTreeElemType;
-typedef struct BinTreeNode
-{
-	BinTreeElemType data;
-	struct BinTreeNode* leftChild;
-	struct BinTreeNode* rightChild;
-}BinTreeNode;
-
-typedef BinTreeNode* BinTree;
-
-//二叉树的创建
-void BinTreeInit(BinTree* t);
-BinTree BinTreeCreate_1();
-void BinTreeInit(BinTree* t)  //BinTreeNode**t;
-{
-	*t = NULL;
-}
-
-BinTree BinTreeCreate_1()
-{
-	BinTreeElemType item;
-	scanf("%c", &item);
-	if (item == '#')
-		return NULL;
-	else
-	{
-		BinTreeNode* t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
-		assert(t != NULL);
-		t->data = item;
-		t->leftChild = BinTreeCreate_1();
-		t->rightChild = BinTreeCreate_1();
-		return t;
-	}
-}
+//#include<assert.h>
+//typedef char BinTreeElemType;
+//typedef struct BinTreeNode
+//{
+//	BinTreeElemType data;
+//	struct BinTreeNode* leftChild;
+//	struct BinTreeNode* rightChild;
+//}BinTreeNode;
+//
+//typedef BinTreeNode* BinTree;
+//
+////二叉树的创建
+//void BinTreeInit(BinTree* t);
+//BinTree BinTreeCreate_1();
+//void BinTreeInit(BinTree* t)  //BinTreeNode**t;
+//{
+//	*t = NULL;
+//}
+//
+//BinTree BinTreeCreate_1()
+//{
+//	BinTreeElemType item;
+//	scanf("%c", &item);
+//	if (item == '#')
+//		return NULL;
+//	else
+//	{
+//		BinTreeNode* t = (BinTreeNode*)malloc(sizeof(BinTreeNode));
+//		assert(t != NULL);
+//		t->data = item;
+//		t->leftChild = BinTreeCreate_1();
+//		t->rightChild = BinTreeCreate_1();
+//		return t;
+//	}
+//}
 void main()
 {
-	const char* str = "ABC##DE##F##G#H##";
-	BinTree bt;
-	BinTreeInit(&bt);
-	bt = BinTreeCreate_1();
+	//char* x = (char*)"abc";	//字符常量区不能变
+	//char* y = (char*)"abc";
+	int ar[] = { 1,2,3 };
+	int* p = ar;
+	*p += 2;
+	*p++;	//这里++优先级更高
+	int* q;
+	q = new int(2);
 	return;
 }
