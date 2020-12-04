11a5970515c3dd9a7d194f0decef11997eaaaf8b
Yfan133 <1157346001@qq.com>
2020-12-04 22:35:48 +0800

    dequeue

--- c++_lesson1/929.cpp
+++ c++_lesson1/929.cpp
@@ -288,5 +288,73 @@ vector和list的共性和差异：
 		vector         list
 共性： 
 
+*/
 
+
+/*
+优先级队列模板参数列表：
+template<class T, class Container = vector<T>, class Com = less<T>>
+priority_queue<int, vector<int>, greater<int>> ar;创建一个小堆
+priority_queue<int, vector<int>, Less> ar;		  创建一个大堆
+prio
+class priority;
+问题：
+	1.为什么使用vector存储元素
+	2.默认按照less对priority_queue中的元素进行比较，创建出来的是一个大堆
+	3.创建小堆，应该怎么设置比较方式
+	设置比较方式：
+		1.函数指针，typedef bool (*Handler_)(Data,Data)
+		2.仿函数(函数对象)：可以像函数一样使用，其本质是一个对象！！
+		3.lambda表达式
+
+bool operator< (const Data a)
+{
+	return 
+}
+问题：如果push的时候传的是地址，那么
+
+
+接口：
+	构造：
+	priority_queue();
+	priority_queue(first, last);	左闭右开
+	操作：
+	void push(const T& data);
+	void pop();
+	const T& top()const; 获取堆顶元素，不能修改，const修饰
+
+*/
+/*
+创建堆：
+1.找调整位置：(size - 1) / 2 ；--->size=nums.size()-1;
+2.for循环，每个位置都向下调整
+3.进入调整函数，记录当前父节点位置，然后向下调整
+	左孩子：left = root * 2 + 1； 
+问题：为啥在删除操作的时候，不直接 while(child > 0 && .....);
+
+每次加<int>是给模板设置类型，
+
+
+1.理解什么是仿函数，怎么使用仿函数
+2.什么是函数指针，怎么使用
+
+模拟实现：
+	容量适配器:
+	stack           ----》vector
+	queue           ----》list
+	priority_queue	----》vector 和 堆算法	
+	都是容量适配器，其实就是将容器封装一下，接口提供给用户，类似(最小栈,最大队列)
+STL实现：stack、queue和priority_queue使用的是deque双端队列
+	deque可进行双端的插入和删除，都是O(1)的时间复杂度
+		问题：连续空间为什么头插，时间复杂度也为O(1)?
+		答：多端连续的空间保证数据的存储，(头插的话反向填充)，再来一个类似Linux中的页表的东西叫map，用来管理地址
+	可以画个图：map存储各个连续空间的首地址，迭代器中有4个指针，分别是cur, first, last, node -> map
+
+双端队列优点：1.插入、删除效率很高
+			  2.扩容效率也很高
+缺点：遍历效率低，但stack和queue都不能遍历，因此STL为了追求高性能采取了deque
+
+为什么STL使用双端队列而不是vector和list
+	对于vector：扩容时优点很大
+	对于queue：
 */
