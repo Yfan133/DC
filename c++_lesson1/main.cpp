e6083d1f5826e276a3bbd86281c64a12d4ec33ca
Yfan133 <1157346001@qq.com>
2020-12-21 23:45:50 +0800

    继承

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -3,7 +3,27 @@
 using namespace std;
 #pragma warning (disable:4996)
 
+
+class par
+{
+	friend void Test();
+private:
+	int a_;
+};
+class chl : public  par
+{
+protected:
+	int b_;
+};
+void Test()
+{
+	chl a;
+	par b;
+	b.a_ = 1;
+	a.a_ = 2;
+}
 int main()
 {
+	Test();
 	return 0;
 }
