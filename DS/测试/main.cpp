6a3b8f1ee8c3ef8eb60091ffa570fdf4f7657b8e
Yfan133 <1157346001@qq.com>
2020-12-25 22:42:44 +0800

    更新

--- DS/测试/main.cpp
+++ DS/测试/main.cpp
@@ -1,11 +1,34 @@
 ﻿#include <iostream>
 #include <set>
 using namespace std;
+#define mod 1000007
+int mi(int x, int t)
+{
+    int r = 1;
+    for (; t; t >>= 1) 
+    {
+        if (t & 1) //为奇数则进去
+            r = (long long)r * x % mod;
+        x = (long long)x * x % mod;
+    }
+    return r;
+}
+int cuttingRope(int n) 
+{
+    if (n < 4)
+        return n - 1;
+    int t = n / 3;
+    int r = n % 3;
+    if (r == 0) 
+        return mi(3, t) % mod;
+    else if (r == 1) 
+        return (long long)mi(3, t - 1) * 4 % mod;
+    else 
+        return (long long)mi(3, t) * 2 % mod;
+}
 int main()
 {
-	set<int> ar;
-	ar.insert(5);
-	if (ar.find(5) == ar.end())
-		printf("hh");
+    mi(3, 5);
+    //cuttingRope(100);
 	return 0;
 }
---
+++ 剑指offer/序列化二叉树2.cpp
@@ -0,0 +1,101 @@
+﻿/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ /**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+    // Encodes a tree to a single string.
+    //队列层序遍历。
+    string serialize(TreeNode * root) {
+        string res  = "";
+        if (root == NULL) return "";
+        //处理一般
+        queue<TreeNode*> my_queue;
+        my_queue.push(root);
+        TreeNode * cur  = new TreeNode(0);
+        while (!my_queue.empty())
+        {
+            //记录队列里的元素长度
+            int len   = my_queue.size();
+            while (len--)
+            {
+                cur  = my_queue.front();
+                my_queue.pop();
+                if (cur == NULL)
+                {
+                    //res  = res + "$";
+                    res.push_back('$');
+                }
+else
+{
+                    //res = res + to_string(cur->val);
+                    res.append(to_string(cur->val));
+               }
+               res.push_back(',');
+               if (cur != NULL)
+               {
+               my_queue.push(cur->left);
+               my_queue.push(cur->right);
+               }
+           }
+       }
+       res.pop_back();
+
+       return res;
+   }
+    // Decodes your encoded data to tree.
+    //重建二叉树。先将节点存起来，然后再遍历给他们建立结构！
+    TreeNode * deserialize(string data) {
+        //处理特殊
+        if (data.size() == 0) return NULL;
+        int len  = data.size();
+        int i  = 0;
+        vector<TreeNode*> vec;
+        while (i < len)
+        {
+            //遇到逗号停下来。
+            string str  = "";
+            while (i < len && data[i] != ',')
+            {
+                str.push_back(data[i]);
+                i++;
+            }
+            //新建根节点.
+            if (str == "$")
+            {
+                TreeNode * temp  = NULL;
+                vec.push_back(temp); //直接存NULL也可以。
+            }
+else {
+   int temp  = std::stoi(str);
+   TreeNode * cur  = new TreeNode(temp);
+   vec.push_back(cur);
+}
+i++;
+}
+        //遍历vec，构建二叉树的结构。
+        int j  = 1;
+        for (int i = 0; j < vec.size(); i++)
+        {
+            if (vec[i] == NULL) continue;
+            if (j < vec.size()) vec[i]->left  = vec[j++];
+            if (j < vec.size()) vec[i]->right  = vec[j++];
+
+        }
+        return vec[0];
+    }
+};
