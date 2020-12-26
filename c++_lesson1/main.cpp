ba4411ab940b6138cb33feb8142cb28427e52ad3
Yfan133 <1157346001@qq.com>
2020-12-26 22:35:27 +0800

    继承多态

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -1,26 +1,23 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+#include <queue>
 using namespace std;
 #pragma warning (disable:4996)
-
-
 class par
 {
-	friend void Test();
-private:
+public:
 	int a_;
 };
 class chl : public  par
 {
-protected:
+public:
 	int b_;
 };
 void Test()
 {
 	chl a;
-	par b;
-	b.a_ = 1;
-	a.a_ = 2;
+	a.a_ = 1;
+	a.b_ = 2;
 }
 int main()
 {
