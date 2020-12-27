ca3a1d44cee02d5cb1996f438b83359627c52846
Yfan133 <1157346001@qq.com>
2020-12-27 22:34:30 +0800

    多态细则

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -1,26 +1,63 @@
 #include <iostream>
 #include <string>
 #include <queue>
+#include <vector>
 using namespace std;
 #pragma warning (disable:4996)
-class par
+class A
 {
 public:
+	virtual void buy()	//只需要基类中的函数加关键字virtual，子类可以不加
+	{
+		cout << "父类" << endl;
+	}
+	virtual A& cov()	//协变：返回值类型不同，但也能构成重写
+	{
+		cout << "cov：父类" << endl;
+		return *this;
+	}
+	~A()
+	{
+		cout << "析构：父类" << endl;
+	}
 	int a_;
 };
-class chl : public  par
+class B : public  A
 {
 public:
-	int b_;
+	B()
+	{
+		b_ = new int[10];
+	}
+	virtual void buy()
+	{
+		cout << "子类" << endl;
+	}
+	virtual B& cov()
+	{
+		cout << "cov：子类" << endl;
+		return *this;
+	}
+	virtual ~B()
+	{
+		cout << "析构：子类" << endl;
+	}
+	int* b_;
 };
-void Test()
+void Test(A& a)			//这个函数通过基类引用或者指针才能体现出多态
 {
-	chl a;
-	a.a_ = 1;
-	a.b_ = 2;
+	a.buy();
+	a.cov();
 }
 int main()
 {
-	Test();
+	A* a = new B;
+	delete a;
+	//A a;
+	//B b;
+	//a.buy();	//这里不能体现多态，因为不满足多态条件：必须是对基类的引用或者指针
+	//b.buy();	
+	//Test(a);
+	//Test(b);
 	return 0;
 }
