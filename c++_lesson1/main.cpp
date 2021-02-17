2f8e762d5061dd694cfb7e18b1b1a6e452a7ab95
Yfan133 <1157346001@qq.com>
2021-02-17 22:45:03 +0800

    更新

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -1,7 +1,7 @@
-//#include <iostream>
-//#include <string.h>
-//#pragma warning (disable:4996)
-//
+#include <iostream>
+#include <string.h>
+#pragma warning (disable:4996)
+
 //namespace old_string
 //{
 //    class string
@@ -109,31 +109,96 @@
 //    Test();
 //    return 0;
 //}
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	// 构造方法
+//	string str1;
+//	string str2("qwer");
+//	string str3(10, 'a');
+//	string str4(str2);
+//	// 容量操作
+//	int size = str2.size();
+//	int length = str2.length();
+//	int capacity = str2.capacity();
+//	str2.reserve(30);
+//	capacity = str2.capacity();
+//	str2.resize(10, 'a');
+//	str2.reserve(20);
+//	capacity = str2.capacity();
+//	str2.reserve(15);
+//	capacity = str2.capacity();
+//	str2.reserve(10);
+//	capacity = str2.capacity();
+//	// 为了提高string类的性能，在内部管理了一个固定大小的数组，大小为16
+//	// 如果reserve new <= 15 ，则容量减小到15
+//	// 如果reserve new > 15 ，则忽略
+//	return 0;
+//}
 
-int main()
+namespace pg_string
 {
-	// 构造方法
-	string str1;
-	string str2("qwer");
-	string str3(10, 'a');
-	string str4(str2);
-	// 容量操作
-	int size = str2.size();
-	int length = str2.length();
-	int capacity = str2.capacity();
-	str2.reserve(30);
-	capacity = str2.capacity();
-	str2.resize(10, 'a');
-	str2.reserve(20);
-	capacity = str2.capacity();
-	str2.reserve(15);
-	capacity = str2.capacity();
-	str2.reserve(10);
-	capacity = str2.capacity();
-	// 为了提高string类的性能，在内部管理了一个固定大小的数组，大小为16
-	// 如果reserve new <= 15 ，则容量减小到15
-	// 如果reserve new > 15 ，则忽略
-	return 0;
+	class string
+	{
+	public:
+		string(const char* str = "")
+		{
+			if (str == nullptr)
+				str = "";
+			_capacity = _size = strlen(str);
+			_str = new char[_size + 1];
+			strcpy(_str, str);
+		}
+		string(size_t n, char ch)
+			:_str(new char[n + 1]), _size(n), _capacity(n)
+		{
+			memset(_str, ch, n);
+			_str[_size] = '\0';
+		}
+		string(const string& s)
+			:_str(nullptr), _size(s.size()),_capacity(s.capacity())
+		{
+			string strTemp(s.c_str());
+			std::swap(_str, strTemp._str);
+		}
+		~string()
+		{
+			delete[] _str;
+			_str = nullptr;
+			_size = _capacity = 0;
+		}
+		string& operator=(string s)
+		{
+			std::swap(_str, s._str);
+			return *this;
+		}
+		size_t size()const
+		{
+			return _size;
+		}
+		size_t capacity()const
+		{
+			return _capacity;
+		}
+		char* c_str()const
+		{
+			return _str;
+		}
+		size_t resize(size_t n, char ch = '\0')
+		{
+
+		}
+		size_t reserve()
+		{
+
+		}
+	private:
+		char* _str;
+		size_t _size;
+		size_t _capacity;
+		static size_t npos;
+	};
+	size_t string::npos = -1;
 }
