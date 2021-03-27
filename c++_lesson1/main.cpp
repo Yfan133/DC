739672b44bef32eff750bb0a6a65219bde47cecf
Yfan133 <1157346001@qq.com>
2021-03-27 22:32:20 +0800

    更新

--- c++_lesson1/main.cpp
+++ c++_lesson1/main.cpp
@@ -40,7 +40,52 @@
 //	return 0;
 //}
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
 
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;
+        cin >> n;
+        vector<int> t(n);
+        vector<int> w(n);
+        for (int i = 0; i < n; ++i)
+            cin >> t[i];
+        for (int i = 0; i < n; ++i)
+            cin >> w[i];
+        // 同一时刻选最大荣耀值
+        unordered_map<int, int> board;
+        int ans = 0;
+        for (int i = 0; i < n; ++i)
+        {
+            if (board.count(t[i]) == 1)
+            {
+                // 重复
+                int value = board[t[i]];
+                if (value > w[i])
+                    ans -= w[i];
+                else
+                {
+                    ans = ans - 2 * value + w[i];
+                    board[t[i]] = w[i];
+                }
+            }
+            else
+            {
+                ans += w[i];
+                board[t[i]] = w[i];
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
 
 
 
